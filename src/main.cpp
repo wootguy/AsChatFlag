f5d57c15b0a40deb703f5b3e795c0164721fd1c9
wootguy <drake.rhunter@gmail.com>
2023-10-15 19:58:08 -0700

    use mmlib

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "meta_init.h"
+#include "mmlib.h"
 #include <string>
 
 // Description of plugin
@@ -22,32 +22,6 @@ enum angelscript_chat_flags {
 
 // cvar that indicates when a chat message is handled by an angelscript plugin
 cvar_t* g_angelscript_chat_flag;
-
-#define MAX_CVARS 1
-cvar_t g_cvar_data[1];
-int g_cvar_count = 0;
-
-#define println(fmt,...) {ALERT(at_console, (char*)(std::string(fmt) + "\n").c_str(), ##__VA_ARGS__); }
-
-cvar_t* RegisterCVar(char* name, char* strDefaultValue, int intDefaultValue, int flags) {
-
-	if (g_cvar_count >= MAX_CVARS) {
-		println("Failed to add cvar '%s'. Increase MAX_CVARS and recompile.", name);
-		return NULL;
-	}
-
-	g_cvar_data[g_cvar_count].name = name;
-	g_cvar_data[g_cvar_count].string = strDefaultValue;
-	g_cvar_data[g_cvar_count].flags = flags | FCVAR_EXTDLL;
-	g_cvar_data[g_cvar_count].value = intDefaultValue;
-	g_cvar_data[g_cvar_count].next = NULL;
-
-	CVAR_REGISTER(&g_cvar_data[g_cvar_count]);
-
-	g_cvar_count++;
-
-	return CVAR_GET_POINTER(name);
-}
 
 void ClientCommand(edict_t* pEntity) {
 	g_angelscript_chat_flag->value = ASCHAT_HANDLED;
